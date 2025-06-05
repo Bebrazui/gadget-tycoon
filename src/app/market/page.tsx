fca23c379768883ebe4d7776cef1945c75dec184
REICK <big.reiks@gmail.com>
2025-06-05 08:55:56 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/market/page.tsx
+++ src/app/market/page.tsx
@@ -1,4 +1,6 @@
 
+"use client";
+
 import { ComingSoon } from "@/components/shared/ComingSoon";
 import { SectionTitle } from "@/components/shared/SectionTitle";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
